3bcc4c4fddc7d9a03eaa9cbc981cbc10b9af3002
Alice Ryhl <aliceryhl@google.com>
2024-08-01 14:16:58 +0000

    rust: add `ARef::into_raw`
    Add a method for `ARef` that is analogous to `Arc::into_raw`. It is the
    inverse operation of `ARef::from_raw`, and allows you to convert the
    `ARef` back into a raw pointer while retaining ownership of the
    refcount.
    
    This new function will be used by [1] for converting the type in an
    `ARef` using `ARef::from_raw(ARef::into_raw(me).cast())`. The author has
    also needed the same function for other use-cases in the past, but [1]
    is the first to go upstream.
    
    Link: https://lore.kernel.org/r/20240801-vma-v3-1-db6c1c0afda9@google.com [1]
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Danilo Krummrich <dakr@kernel.org>
    Link: https://lore.kernel.org/r/20240801-aref-into-raw-v1-1-33401e2fbac8@google.com

--- rust/kernel/types.rs
+++ rust/kernel/types.rs
@@ -7,7 +7,7 @@ use alloc::boxed::Box;
 use core::{
     cell::UnsafeCell,
     marker::{PhantomData, PhantomPinned},
-    mem::MaybeUninit,
+    mem::{ManuallyDrop, MaybeUninit},
     ops::{Deref, DerefMut},
     ptr::NonNull,
 };
@@ -410,6 +410,14 @@ impl<T: AlwaysRefCounted> ARef<T> {
             ptr,
             _p: PhantomData,
         }
+    }
+
+    /// Consumes the `ARef`, returning a raw pointer.
+    ///
+    /// This function does not change the refcount. After calling this function, the caller is
+    /// responsible for the refcount previously managed by the `ARef`.
+    pub fn into_raw(me: Self) -> NonNull<T> {
+        ManuallyDrop::new(me).ptr
     }
 }
 
