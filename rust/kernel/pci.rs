8b71d5eeb966858a05d639b9266f4422dc3a5647
Danilo Krummrich <dakr@kernel.org>
2024-08-22 02:54:37 +0200

    rust: pci: add initial `DeviceId` abstraction
    Add the code to safely register NULL terminated PCI device ID tables for an
    arbitrary number of devices.
    
    This doesn't support driver data yet and ignores that fact that we can
    generalize this abstraction for all subsystems / busses.
    
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>

--- rust/kernel/pci.rs
+++ rust/kernel/pci.rs
@@ -11,13 +11,144 @@ use kernel::{
     prelude::*,
 };
 
+/// Abstraction for `bindings::pci_device_id`.
+#[derive(Clone, Copy)]
+pub struct DeviceId {
+    /// Vendor ID
+    pub vendor: u32,
+    /// Device ID
+    pub device: u32,
+    /// Subsystem vendor ID
+    pub subvendor: u32,
+    /// Subsystem device ID
+    pub subdevice: u32,
+    /// Device class and subclass
+    pub class: u32,
+    /// Limit which sub-fields of the class
+    pub class_mask: u32,
+}
+
+impl DeviceId {
+    /// Zeroed `bindings::pci_device_id`.
+    // SAFETY; The all-zero byte-pattern is valid for `bindings::pci_device_id`.
+    pub const ZERO: bindings::pci_device_id = unsafe { core::mem::zeroed() };
+    const PCI_ANY_ID: u32 = !0;
+
+    /// Equivalent to the PCI_DEVICE macro.
+    pub const fn new(vendor: u32, device: u32) -> Self {
+        Self {
+            vendor,
+            device,
+            subvendor: DeviceId::PCI_ANY_ID,
+            subdevice: DeviceId::PCI_ANY_ID,
+            class: 0,
+            class_mask: 0,
+        }
+    }
+
+    /// Convert `DeviceId` to raw `bindings::pci_device_id`.
+    pub const fn to_rawid(&self) -> bindings::pci_device_id {
+        let mut raw = Self::ZERO;
+
+        raw.vendor = self.vendor;
+        raw.device = self.device;
+        raw.subvendor = self.subvendor;
+        raw.subdevice = self.subdevice;
+        raw.class = self.class;
+        raw.class_mask = self.class_mask;
+
+        raw
+    }
+}
+
+/// A zero-terminated PCI device ID array.
+#[repr(C)]
+pub struct IdArray<const N: usize> {
+    ids: [bindings::pci_device_id; N],
+    sentinel: bindings::pci_device_id,
+}
+
+impl<const N: usize> IdArray<N> {
+    /// Creates a new instance of the ID array.
+    ///
+    /// The contents are derived from the given identifiers.
+    #[doc(hidden)]
+    pub const fn new(ids: [bindings::pci_device_id; N]) -> Self {
+        Self {
+            ids,
+            sentinel: DeviceId::ZERO,
+        }
+    }
+
+    /// Returns an `IdTable` backed by `self`.
+    ///
+    /// This is used to essentially erase the array size.
+    pub const fn as_table(&self) -> IdTable<'_> {
+        IdTable {
+            first: &self.ids[0],
+        }
+    }
+}
+
+/// A device ID table.
+///
+/// The table is guaranteed to be zero-terminated.
+#[repr(C)]
+pub struct IdTable<'a> {
+    first: &'a bindings::pci_device_id,
+}
+
+impl AsRef<bindings::pci_device_id> for IdTable<'_> {
+    fn as_ref(&self) -> &bindings::pci_device_id {
+        self.first
+    }
+}
+
+/// Counts the number of parenthesis-delimited, comma-separated items.
+#[macro_export]
+macro_rules! count_paren_items {
+    (($($item:tt)*), $($remaining:tt)*) => { 1 + $crate::count_paren_items!($($remaining)*) };
+    (($($item:tt)*)) => { 1 };
+    () => { 0 };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! define_pci_id_array {
+    ($($args:tt)*) => {{
+        const fn new<const N: usize>(ids: [$crate::pci::DeviceId; N]) -> $crate::pci::IdArray<N> {
+            let mut raw_ids = [$crate::pci::DeviceId::ZERO; N];
+
+            let mut i = 0usize;
+            while i < N {
+                raw_ids[i] = ids[i].to_rawid();
+                i += 1;
+            }
+
+            $crate::pci::IdArray::<N>::new(raw_ids)
+        }
+
+        new([ $($args)* ])
+    }}
+}
+
+/// Define a const PCI device ID table.
+#[macro_export]
+macro_rules! define_pci_id_table {
+    ([ $($args:tt)* ]) => {
+        const ID_TABLE: $crate::pci::IdTable<'static> = {
+            const ARRAY: $crate::pci::IdArray<{ $crate::count_paren_items!($($args)*) }> =
+                $crate::define_pci_id_array!($($args)*);
+            ARRAY.as_table()
+        };
+    };
+}
+pub use define_pci_id_table;
+
 /// Drivers must implement this trait to register a PCI driver.
 pub trait Driver {
-    /// Pointer to the PCI driver's ID table.
-    ///
-    /// This is highly unsafe, since we have to trust the driver, that the provided pointer is
-    /// valid.
-    const ID_TABLE: *const bindings::pci_device_id;
+    /// The table of device IDs supported by this driver.
+    const ID_TABLE: IdTable<'static>;
 
     /// PCI driver probe.
     ///
@@ -70,7 +201,7 @@ where
         pdrv.name = name.as_char_ptr();
         pdrv.probe = Some(Self::probe);
         pdrv.remove = Some(Self::remove);
-        pdrv.id_table = T::ID_TABLE;
+        pdrv.id_table = T::ID_TABLE.as_ref();
 
         // SAFETY: `pdrv` is a valid `struct pci_driver`; `ThisModule` is equivalent to
         // C's `THIS_MODULE` and hence valid for `__pci_register_driver`. `name` is passed as `NULL`
--- samples/rust/rust_pci_driver/driver.rs
+++ samples/rust/rust_pci_driver/driver.rs
@@ -2,34 +2,17 @@
 
 //! Rust PCI driver sample
 
-use kernel::{bindings, pci, prelude::*};
+use kernel::{bindings, pci, pci::define_pci_id_table, prelude::*};
 
 const PCI_DEVICE_ID_REDHAT_QEMU_PCI_TESTDEV: u32 = 0x0005;
 
 pub(crate) struct Driver;
 
-impl Driver {
-    const IDS: usize = 2;
-    const __ID_TABLE: [bindings::pci_device_id; Self::IDS] = Self::id_table();
-
-    const fn id_table() -> [bindings::pci_device_id; Self::IDS] {
-        // SAFETY: `bindings::pci_device_id` is valid to be zero initialized.
-        let mut id: bindings::pci_device_id = unsafe { core::mem::zeroed() };
-
-        id.vendor = bindings::PCI_VENDOR_ID_REDHAT;
-        id.device = PCI_DEVICE_ID_REDHAT_QEMU_PCI_TESTDEV;
-        id.subvendor = bindings::PCI_ANY_ID as u32;
-        id.subdevice = bindings::PCI_ANY_ID as u32;
-
-        // SAFETY: `bindings::pci_device_id` is valid to be zero initialized.
-        let sentinel: bindings::pci_device_id = unsafe { core::mem::zeroed() };
-
-        [id, sentinel]
-    }
-}
-
 impl pci::Driver for Driver {
-    const ID_TABLE: *const bindings::pci_device_id = Self::__ID_TABLE.as_ptr();
+    define_pci_id_table! {
+        [ (pci::DeviceId::new(bindings::PCI_VENDOR_ID_REDHAT,
+                             PCI_DEVICE_ID_REDHAT_QEMU_PCI_TESTDEV)) ]
+    }
 
     fn probe(_pdev: *mut bindings::pci_dev) -> Result {
         pr_info!("Probe Rust PCI driver sample.\n");
