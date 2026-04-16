111b364a04ea7c7330681a7d9545ff6c44abb5f6
Gary Guo <gary@garyguo.net>
2026-04-16 10:48:15 +0100

    rust: types: add `ForLt` trait for higher-ranked lifetime support
    There are a few cases, e.g. when dealing with data referencing each other,
    one might want to write code that are generic over lifetimes. For example,
    if you want take a function that takes `&'a Foo` and gives `Bar<'a>`, you
    can write:
    
        f: impl for<'a> FnOnce(&'a Foo) -> Bar<'a>,
    
    However, it becomes tricky when you want that function to not have a fixed
    `Bar`, but have it be generic again. In this case, one needs something that
    is generic over types that are themselves generic over lifetimes.
    
    `ForLt` provides such support. It provides a trait `ForLt` which describes
    a type generic over lifetime. One may use `ForLt::Of<'a>` to get an
    instance of a type for a specific lifetime.
    
    For the case of cross referencing, one would almost always want the
    lifetime to be covariant. Therefore this is also made a requirement for the
    `ForLt` trait, so functions with `ForLt` trait bound can assume covariance.
    
    A macro `ForLt!()` is provided to be able to obtain a type that implements
    `ForLt`. For example, `ForLt!(for<'a> Bar<'a>)` would yield a type that
    `<TheType as ForLt>::Of<'a>` is `Bar<'a>`. This also works with lifetime
    elision, e.g. `ForLt!(Bar<'_>)` or for types without lifetime at all, e.g.
    `ForLt!(u32)`.
    
    The API design draws inspiration from the higher-kinded-types [1] crate,
    however different design decision has been taken (e.g. covariance
    requirement) and the implementation is independent.
    
    License headers use "Apache-2.0 OR MIT" because I anticipate this to be
    used in pin-init crate too which is licensed as such.
    
    Link: https://docs.rs/higher-kinded-types/ [1]
    
    Signed-off-by: Gary Guo <gary@garyguo.net>

--- rust/kernel/types.rs
+++ rust/kernel/types.rs
@@ -13,6 +13,10 @@ use pin_init::{PinInit, Wrapper, Zeroable};
 
 pub use crate::sync::aref::{ARef, AlwaysRefCounted};
 
+#[doc(hidden)]
+pub mod for_lt;
+pub use for_lt::ForLt;
+
 /// Used to transfer ownership to and from foreign (non-Rust) languages.
 ///
 /// Ownership is transferred from Rust to a foreign language by calling [`Self::into_foreign`] and
