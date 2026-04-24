4da5f71e7393f50f1cd8111eb736c07113e33708
Danilo Krummrich <dakr@kernel.org>
2026-04-25 01:42:08 +0200

    rust: usb: make Driver trait lifetime-parameterized
    Make usb::Driver take a lifetime parameter 'bound that ties device
    resources to the binding scope.
    
    Internally, Adapter<T: Driver> becomes Adapter<F: ForLt> with a bound
    for<'bound> F::Of<'bound>: Driver<'bound>; module_usb_driver! wraps the
    driver type in ForLt!() so drivers don't have to.
    
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>

--- samples/rust/rust_driver_usb.rs
+++ samples/rust/rust_driver_usb.rs
@@ -20,26 +20,26 @@ struct SampleDriver {
 kernel::usb_device_table!(
     USB_TABLE,
     MODULE_USB_TABLE,
-    <SampleDriver as usb::Driver>::IdInfo,
+    <SampleDriver as usb::Driver<'_>>::IdInfo,
     [(usb::DeviceId::from_id(0x1234, 0x5678), ()),]
 );
 
-impl usb::Driver for SampleDriver {
+impl<'bound> usb::Driver<'bound> for SampleDriver {
     type IdInfo = ();
     const ID_TABLE: usb::IdTable<Self::IdInfo> = &USB_TABLE;
 
     fn probe(
-        intf: &usb::Interface<Core>,
-        _id: &usb::DeviceId,
-        _info: &Self::IdInfo,
-    ) -> impl PinInit<Self, Error> {
+        intf: &'bound usb::Interface<Core>,
+        _id: &'bound usb::DeviceId,
+        _info: &'bound Self::IdInfo,
+    ) -> impl PinInit<Self, Error> + 'bound {
         let dev: &device::Device<Core> = intf.as_ref();
         dev_info!(dev, "Rust USB driver sample probed\n");
 
         Ok(Self { _intf: intf.into() })
     }
 
-    fn disconnect(intf: &usb::Interface<Core>, _data: Pin<&Self>) {
+    fn disconnect(intf: &'bound usb::Interface<Core>, _data: Pin<&'bound Self>) {
         let dev: &device::Device<Core> = intf.as_ref();
         dev_info!(dev, "Rust USB driver sample disconnected\n");
     }
